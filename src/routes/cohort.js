d0d88323e64b2b9f2f9ef89b979d85f75f731920
Jone <64496635+JoneTheBuilder@users.noreply.github.com>
2025-02-26 14:36:31 +0100

    Implemented API - Retrieve users by cohort #8

--- src/routes/cohort.js
+++ src/routes/cohort.js
@@ -1,12 +1,19 @@
 import { Router } from 'express'
-import { create } from '../controllers/cohort.js'
+import { create, getCohortMembers } from '../controllers/cohort.js'
 import {
   validateAuthentication,
-  validateTeacherRole
+  validateTeacherRole,
+  validateCohortAccess
 } from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/', validateAuthentication, validateTeacherRole, create)
+router.get(
+  '/:id/members',
+  validateAuthentication,
+  validateCohortAccess,
+  getCohortMembers
+)
 
 export default router
